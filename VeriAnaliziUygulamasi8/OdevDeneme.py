c48dfffdd72ddfc58eedf191deea57fe23e6ceca
Muhsin DOLU <muhsindolu06@gmail.com>
2025-01-29 21:28:48 +0300

    Konut fiyatı tahmini için veri analizi
    Konut Fiyatlarının Tahmini için Spark ile Regresyon Modeli Oluşturulması

--- VeriAnaliziUygulamasi8/OdevDeneme.py
+++ VeriAnaliziUygulamasi8/OdevDeneme.py
@@ -1,10 +1,44 @@
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
+# Veri kümesinin okunması
 df=pd.read_csv("C:/Users/Muhsin/Desktop/Milli Teknoloji Hamlesi TUBİTAK Ödev/housing.csv")
+
+# Sütun isimleri
+print("Sütun isimleri: ")
+print(df.columns)
+print("-"*50)
+
+# Veri kümesi dağılımı
+print("Veri kümesi dağılımı: ")
+print(df.describe())
+print("-"*50)
 
 print("Bilgi: ")
 print(df.info())
 print("-"*50)
-print(df.describe())
+# object tipinde tek sütun var bu sütun ise "ocean_proximity"
+
+print("Sütunda ki çeşitli değerler: ")
+print(df.ocean_proximity.unique())
+print("-"*50)
+# 'NEAR BAY' '<1H OCEAN' 'INLAND' 'NEAR OCEAN' 'ISLAND' olmak üzere 4 çeşit str değer içeriyor
+
+
+
+
+
+
+
+# Null Değer kontrolü
 print("NaN değer var mı?")
 print(df.isnull().sum())
+print("-"*50)
+# total_bedrooms sütununda 207 adet NaN değer var
+
+
+
+
+
+
